14e3b00aa31bda78e44a3f1bb00e52954ef860f8
longgb246 <lgb453476610@163.com>
2018-12-10 09:47:35 +0800

    脚本更新，学习

--- kaggle/talkingdata_ad_track/scripts_1.py
+++ kaggle/talkingdata_ad_track/scripts_1.py
@@ -75,16 +75,121 @@ df_test = pd.read_csv(org_path + os.sep + 'test.csv', nrows=1000000)
 cols = ['ip', 'app', 'device', 'os', 'channel']
 uniques = [len(df_train[col].unique()) for col in cols]
 
-import seaborn as sns
-
-plt.figure(figsize=(15, 8))
-sns.set(font_scale=1.2)
-# ax = sns.barplot(cols, uniques, palette=pal, log=True)
-ax = sns.barplot(cols, uniques, log=True)
-ax.set(xlabel='Feature', ylabel='log(unique count)', title='Number of unique values per feature')
-for p, uniq in zip(ax.patches, uniques):
-    height = p.get_height()
-    ax.text(p.get_x() + p.get_width() / 2., height + 10, uniq, ha="center")
+# import seaborn as sns
+#
+# plt.figure(figsize=(15, 8))
+# sns.set(font_scale=1.2)
+# # ax = sns.barplot(cols, uniques, palette=pal, log=True)
+# ax = sns.barplot(cols, uniques, log=True)
+# ax.set(xlabel='Feature', ylabel='log(unique count)', title='Number of unique values per feature')
+# for p, uniq in zip(ax.patches, uniques):
+#     height = p.get_height()
+#     ax.text(p.get_x() + p.get_width() / 2., height + 10, uniq, ha="center")
 
 lpt.barplot(cols, uniques, log=True)
 lpt.barplot(cols, uniques)
+
+# ip, app, device, os and channel are actually categorical variables encoded as integers. Set them as categories for analysis.
+variables = ['ip', 'app', 'device', 'os', 'channel']
+for v in variables:
+    df_train[v] = df_train[v].astype('category')
+    df_test[v] = df_test[v].astype('category')
+
+train = df_train
+test = df_test
+
+# Convert date stamps to date/time type.
+# set click_time and attributed_time as timeseries
+train['click_time'] = pd.to_datetime(train['click_time'])
+train['attributed_time'] = pd.to_datetime(train['attributed_time'])
+test['click_time'] = pd.to_datetime(test['click_time'])
+
+# set as_attributed in train as a categorical
+train['is_attributed'] = train['is_attributed'].astype('category')
+
+# Now lets do a quick inspection of train and test data main statistics
+train.describe()
+
+# double check that 'attributed_time' is not Null for all values that resulted in download (i.e. is_attributed == 1)
+train[['attributed_time', 'is_attributed']][train['is_attributed'] == 1].describe()
+
+# set click_id to categorical, for cleaner statistics view
+test['click_id'] = test['click_id'].astype('category')
+test.describe()
+
+# temporary table to see ips with their associated count frequencies
+temp = train['ip'].value_counts().reset_index(name='counts')
+temp.columns = ['ip', 'counts']
+temp[:10]
+
+# add temporary counts of ip feature ('counts') to the train table, to see if IPs with high counts have conversions
+train = train.merge(temp, on='ip', how='left')
+# check top 10 values
+train[train['is_attributed'] == 1].sort_values('counts', ascending=False)[:10]
+
+# convert 'is_attributed' back to numeric for proportion calculations
+train['is_attributed'] = train['is_attributed'].astype(int)
+
+# 检查 y 与一些 x 之间的关系。是否有直接的关系
+# Conversion rates over Counts of 300 most popular IPs
+
+train_smp = pd.read_csv(org_path + os.sep + 'train.csv', nrows=1000000)
+# convert click_time and attributed_time to time series
+train_smp['click_time'] = pd.to_datetime(train_smp['click_time'])
+train_smp['attributed_time'] = pd.to_datetime(train_smp['attributed_time'])
+
+# round the time to nearest hour
+train_smp['click_rnd'] = train_smp['click_time'].dt.round('H')
+
+# check for hourly patterns
+train_smp[['click_rnd', 'is_attributed']].groupby(['click_rnd'], as_index=True).count().plot()
+plt.title('HOURLY CLICK FREQUENCY')
+plt.ylabel('Number of Clicks')
+
+train_smp[['click_rnd', 'is_attributed']].groupby(['click_rnd'], as_index=True).mean().plot()
+plt.title('HOURLY CONVERSION RATIO')
+plt.ylabel('Converted Ratio')
+
+# extract hour as a feature
+train_smp['click_hour'] = train_smp['click_time'].dt.hour
+
+train_smp[['click_hour', 'is_attributed']].groupby(['click_hour'], as_index=True).count(). \
+    plot(kind='bar', color='#a675a1')
+plt.title('HOURLY CLICK FREQUENCY Barplot')
+plt.ylabel('Number of Clicks')
+
+# adapted from https://stackoverflow.com/questions/9103166/multiple-axis-in-matplotlib-with-different-scales
+# smonek's answer
+group = train_smp[['click_hour', 'is_attributed']].groupby(['click_hour'], as_index=False).mean()
+x = group['click_hour']
+ymean = group['is_attributed']
+group = train_smp[['click_hour', 'is_attributed']].groupby(['click_hour'], as_index=False).count()
+ycount = group['is_attributed']
+
+fig = plt.figure()
+host = fig.add_subplot(111)
+
+par1 = host.twinx()
+host.set_xlabel("Time")
+host.set_ylabel("Proportion Converted")
+par1.set_ylabel("Click Count")
+# color1 = plt.cm.viridis(0)
+# color2 = plt.cm.viridis(0.5)
+color1 = '#75a1a6'
+color2 = '#a675a1'
+p1, = host.plot(x, ymean, color=color1, label="Proportion Converted")
+p2, = par1.plot(x, ycount, color=color2, label="Click Count")
+
+lns = [p1, p2]
+host.legend(handles=lns, loc='best')
+
+host.yaxis.label.set_color(p1.get_color())
+par1.yaxis.label.set_color(p2.get_color())
+
+plt.savefig("pyplot_multiple_y-axis.png", bbox_inches='tight')
+
+# beautiful
+
+sns.barplot('click_hour', 'is_attributed', data=train_smp)
+plt.title('HOURLY CONVERSION RATIO')
+plt.ylabel('Converted Ratio')
